9f00438d67bcf1b4914aa064ff4fa94fd0388a2a
Alexey <248997@gmail.com>
2023-12-10 19:38:13 +0500

    feat: add build and publishing scripts
    These build.bat and publish_nuget.bat scripts have been created for building and publishing the project. The scripts automatically update the project version using git describe and perform necessary operations for both build and publish tasks.

---
+++ publish_nuget.bat
@@ -0,0 +1,23 @@
+@echo off
+rem ====== projects ======
+
+set projects=Asv.Avalonia.Toolkit
+
+rem ====== projects ======
+
+rem copy version to text file, then in variable
+git describe --abbrev=0 >./version.txt
+SET /p VERSION=<version.txt
+DEL version.txt
+
+(for %%p in (%projects%) do (
+	cd src\%%p\bin\Release\
+	dotnet nuget push %%p.%VERSION:~1%.nupkg --skip-duplicate --source https://api.nuget.org/v3/index.json
+	dotnet nuget push %%p.%VERSION:~1%.nupkg --skip-duplicate --source https://nuget.pkg.github.com/asv-soft/index.json
+	cd ../../../../
+)) 
+
+
+
+
+
