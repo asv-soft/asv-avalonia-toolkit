2251498856f7e6a21ec7dd82f42329245a3f8ea0
Eugene <u03@asv.me>
2024-02-12 10:42:10 +0300

    feat: add theme switch function

---
+++ src/Asv.Avalonia.ToolkitGallery/Theme/IThemeService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Asv.Common;
+using Avalonia.Media;
+using Avalonia.Styling;
+
+namespace Asv.Drones.Gui.Core
+{
+    public class ThemeItem
+    {
+        public ThemeItem(string id, string name,ThemeVariant theme)
+        {
+            Id = id;
+            Name = name;
+            Theme = theme;
+        }
+
+        public string Id { get; }
+        public string Name { get; }
+        public ThemeVariant Theme { get; }
+    }
+
+
+    public interface IThemeService
+    {
+        IEnumerable<ThemeItem> Themes { get; }
+        IRxEditableValue<ThemeItem> CurrentTheme { get; }
+    }
+}
