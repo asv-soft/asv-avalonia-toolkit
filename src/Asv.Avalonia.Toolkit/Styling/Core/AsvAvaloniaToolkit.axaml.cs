bfa270e69a15d59ebe39c872fcfd612fd5d0e182
u02 <u02@asv.me>
2024-02-25 18:21:35 +0300

    feat: remove FluentAvalonia links, add AsvAvaloniaToolkit class
    Removed FluentAvalonia references and introduced AsvAvaloniaToolkit as the primary class for cleaner project integration. Previously, errors might occur due to incorrectly written absolute paths. The new main class also allows for future functionality expansion and parameter addition.
    
    Asana: https://app.asana.com/0/1203851531040615/1206687927869186/f

---
+++ src/Asv.Avalonia.Toolkit/Styling/Core/AsvAvaloniaToolkit.axaml.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+
+namespace Asv.Avalonia.Toolkit.Styling.Core;
+
+/// <summary>
+/// Theme manager for FluentAvalonia, managing various components of the Fluentv2 theme
+/// like AccentColor, styles, and platform settings
+/// </summary>
+public partial class AsvAvaloniaToolkit : Styles, IResourceProvider
+{
+    /// <summary>
+    /// Create new instance of <see cref="AsvAvaloniaToolkit"/>.
+    /// </summary>
+    public AsvAvaloniaToolkit()
+    {
+        Init();
+    }
+    
+    private void Init()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
