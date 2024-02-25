088a55722b9d004d633bde41bb2364966acb0068
Alexey <248997@gmail.com>
2024-02-25 14:20:19 +0500

    feat: add solution items and build scripts

--- build.bat
+++ build.bat
@@ -5,11 +5,19 @@ set projects=Asv.Avalonia.Toolkit
 
 rem ====== projects ======
 
+rem copy version to text file, then in variable
+git describe --tags --abbrev=4 >>version.txt
+SET /p VERSION=<version.txt
+del version.txt
+
+rem Extracting only X.X.X from the version string
+SET VERSION=%VERSION:v=%
+
 rem build all projects
 (for %%p in (%projects%) do (
   	echo %%p
 	dotnet restore ./src/%%p/%%p.csproj
-	dotnet build ./src/%%p/%%p.csproj -c Release
+	dotnet build /p:SolutionDir=../;ProductVersion=%VERSION% ./src/%%p/%%p.csproj -c Release
 	dotnet pack ./src/%%p/%%p.csproj -c Release
 )) 
 
--- publish_nuget.bat
+++ publish_nuget.bat
@@ -5,15 +5,18 @@ set projects=Asv.Avalonia.Toolkit
 
 rem ====== projects ======
 
-rem copy version to text file, then in variable
-git describe --tags --abbrev=0 >./version.txt
+ rem copy version to text file, then in variable
+git describe --tags --abbrev=4 >>version.txt
 SET /p VERSION=<version.txt
-DEL version.txt
+del version.txt
+
+rem Extracting only X.X.X from the version string
+SET VERSION=%VERSION:v=%
 
 (for %%p in (%projects%) do (
 	cd src\%%p\bin\Release\
-	dotnet nuget push %%p.%VERSION:~1%.nupkg --skip-duplicate --source https://api.nuget.org/v3/index.json
-	dotnet nuget push %%p.%VERSION:~1%.nupkg --skip-duplicate --source https://nuget.pkg.github.com/asv-soft/index.json
+	dotnet nuget push%%p.%VERSION%.nupkg --skip-duplicate --source https://api.nuget.org/v3/index.json
+	dotnet nuget %%p.%VERSION%.nupkg --skip-duplicate --source https://nuget.pkg.github.com/asv-soft/index.json
 	cd ../../../../
 )) 
 
