57bfc7908893539502149da713d2fef3609f3dc0
Alexey <248997@gmail.com>
2024-02-25 14:07:30 +0500

    feat: move to net8.0 framework

--- build.bat
+++ build.bat
@@ -5,18 +5,9 @@ set projects=Asv.Avalonia.Toolkit
 
 rem ====== projects ======
 
-rem install tool for update project version by git describe
-dotnet tool install -g dotnet-setversion
-
-rem copy version to text file, then in variable
-git describe --tags --abbrev=0 >./version.txt
-SET /p VERSION=<version.txt
-DEL version.txt
-
 rem build all projects
 (for %%p in (%projects%) do (
   	echo %%p
-	setversion %VERSION:~1% ./src/%%p/%%p.csproj
 	dotnet restore ./src/%%p/%%p.csproj
 	dotnet build ./src/%%p/%%p.csproj -c Release
 	dotnet pack ./src/%%p/%%p.csproj -c Release
