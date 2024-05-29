2b49eafe8b8829b34f439782832c0d0d0e670bbd
Eugene <u03@asv.me>
2024-05-29 14:38:34 +0300

    feat(step-sizing-button): some code refactor

--- src/Asv.Avalonia.Toolkit/UI/Controls/Buttons/StepSizingButton.cs
+++ src/Asv.Avalonia.Toolkit/UI/Controls/Buttons/StepSizingButton.cs
@@ -10,25 +10,31 @@ public class StepSizingButton : Button
     public static readonly StyledProperty<double> ButtonUnitWidthProperty =
         AvaloniaProperty.Register<StepSizingButton, double>(nameof(StepSizeWidth));
 
+    /// <summary>
+    /// Get or Set step of width for control
+    /// </summary>
     public double StepSizeWidth
     {
         get => GetValue(ButtonUnitWidthProperty);
         set => SetValue(ButtonUnitWidthProperty, value);
     }
-    
+
     public static readonly StyledProperty<double> ButtonUnitHeightProperty =
         AvaloniaProperty.Register<StepSizingButton, double>(nameof(StepSizeHeight));
 
+    /// <summary>
+    /// Get or Set step of height for control
+    /// </summary>
     public double StepSizeHeight
     {
         get => GetValue(ButtonUnitHeightProperty);
         set => SetValue(ButtonUnitHeightProperty, value);
     }
-    
+
     protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
     {
         base.OnPropertyChanged(change);
-        
+
         if (change.Property == BoundsProperty)
         {
             MinWidth = StepSizeWidth;
@@ -37,25 +43,22 @@ public class StepSizingButton : Button
                 if (Bounds.Width > StepSizeWidth)
                 {
                     var increment = (int)Math.Round(Bounds.Width / StepSizeWidth);
-                    Width = StepSizeWidth * increment + (Margin.Right+Margin.Left) * (increment-1);
+                    Width = StepSizeWidth * increment + (Margin.Right + Margin.Left) * (increment - 1);
                 }
                 else
                 {
                     Width = StepSizeWidth;
                 }
             }
-
-            if (StepSizeHeight != 0)
+            if (StepSizeHeight == 0) return;
+            if (Bounds.Height > StepSizeHeight)
             {
-                if (Bounds.Height > StepSizeHeight)
-                {
-                    var increment = (int)Math.Round(Bounds.Height / StepSizeHeight);
-                    Height = StepSizeHeight * increment + (Margin.Top+Margin.Bottom) * (increment-1);
-                }
-                else
-                {
-                    Height = StepSizeHeight;
-                }
+                var increment = (int)Math.Round(Bounds.Height / StepSizeHeight);
+                Height = StepSizeHeight * increment + (Margin.Top + Margin.Bottom) * (increment - 1);
+            }
+            else
+            {
+                Height = StepSizeHeight;
             }
         }
     }
